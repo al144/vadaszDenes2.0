c2a373afeb3ca41fcc452aba2d7f3f6ee364928c
al144 <davidasztral2007@gmail.com>
2026-03-06 23:35:47 +0100

    Add project files.

---
+++ Rover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vadaszDenes
+{
+    class Rover
+    {
+        enum Action{
+            MovingSlow,
+            MovingNormal,
+            MovingFast,
+            Mining,
+            
+
+        }
+
+        private string name { get; set; }
+        private int[] startPos { get; set; }
+        private int[] currentPos { get; set; }
+        private int battery { get; set; }
+
+
+        public Rover(string name) { this.name = name; }
+
+        public Rover(string[] data)
+        {
+
+        }
+    }
+}
